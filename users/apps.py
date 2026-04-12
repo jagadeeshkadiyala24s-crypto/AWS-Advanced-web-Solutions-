2ec72c7e790d37e2168f68e45ef891a6b0605e14
Sekhar Gudivada <bharathsekhargudivada@gmail.com>
2026-04-12 20:12:09 +0100

    Delete users directory

--- users/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-<<<<<<< HEAD
-=======
-
-
->>>>>>> e8972cb74228f0025e74b408502006ef45737c8c
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
