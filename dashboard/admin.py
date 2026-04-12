3f71c5c612b21bae101e8cc0a90718bdcb5b92e6
Sekhar Gudivada <bharathsekhargudivada@gmail.com>
2026-04-12 20:11:03 +0100

    Delete dashboard directory

--- dashboard/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ClinicianProfile, PatientProfile, PressureSession, PressureFrame, PatientAlert, FrameComment
-
-admin.site.register(ClinicianProfile)
-admin.site.register(PatientProfile)
-admin.site.register(PressureSession)
-admin.site.register(PressureFrame)
-admin.site.register(PatientAlert)
-admin.site.register(FrameComment)
